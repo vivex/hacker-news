53087cf540e8f64d75e7d9cda9dfd6f622515288
Vivek Soni <vicky.soni@ymail.com>
2020-07-06 01:29:17 +0530

    Fixes lint errors

--- src/modules/News/news.api.js
+++ src/modules/News/news.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_HOST = 'http://localhost:3000/api/v1';
 
-export const fetchStories = async ({query = '', tags='story', page}) => {
+export const fetchStories = async ({query = '', tags='story', page=0}) => {
     let response = await axios({url: `${API_HOST}/search?query=${query}&tags=${tags}&page=${page}`, responseType: 'json'});
     return response.data;
 }
