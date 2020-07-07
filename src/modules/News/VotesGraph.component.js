f06608e6d3bbec9d40b6fa8dbb9b3f137eb5f938
Vivek Soni <vicky.soni@ymail.com>
2020-07-07 10:24:14 +0530

    Chart responsiveness

--- src/modules/News/VotesGraph.component.js
+++ src/modules/News/VotesGraph.component.js
@@ -1,12 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import LineChart from 'react-linechart';
 import '../../../node_modules/react-linechart/dist/styles.css';
 
 const VotesGraph = ({news}) => {
+  let windowWidth = typeof window!== 'undefined' ?  window.innerWidth : 300;
+  if (windowWidth && windowWidth > 1200)
+    windowWidth = 1200;
 
+  const [width, setWidth] = useState(null);
   useEffect(()=> {
-
-  }, [news]);
+    setWidth(windowWidth);
+  }, [windowWidth]);
 
   const data = [
     {
@@ -14,15 +18,12 @@ const VotesGraph = ({news}) => {
       points: []
     }
   ];
-  console.log('news', news);
   if (news && news.hits) {
     news.hits.forEach((n)=> {
       data[0].points.push({y: n.objectID*1, x: n.points});
     });
   }
-  let windowWidth = typeof window!== 'undefined' ?  window.innerWidth : null;
-  if (windowWidth && windowWidth > 1200)
-    windowWidth = 1200
+
 
   return (
     <div className="c-votes-graph">
@@ -31,7 +32,7 @@ const VotesGraph = ({news}) => {
           xLabel="Votes"
           yLabel="Id"
           height={400}
-          width={windowWidth}
+          width={width}
           data={data}
         />
       </div>
